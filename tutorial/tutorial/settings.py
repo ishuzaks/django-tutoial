772a3d62306b4b5a37f25ef81a0bdea07199c718
ishuzaks <k.suzuki512+develop@gmail.com>
2021-09-19 05:54:11 +0000

    Hide Django sercet key.

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+try:
+    from .local_settings import SECRET_KEY as IMPORTED_SECRET_KEY
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=gqs(_*bnjf0#bjdirl3i!u$l#@-qbu%94@fy_t36(v$&!n06k'
+SECRET_KEY = IMPORTED_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
